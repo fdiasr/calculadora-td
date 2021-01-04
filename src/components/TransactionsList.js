7e2cd8ca87eb1432876d0e8a12a8f30b1a31fc4f
Fernando Dias <fdiasdev@gmail.com>
2021-01-04 00:57:02 -0300

    Implements callbacks in TransactionsList

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -4,35 +4,37 @@ import { v4 as uuidv4 } from 'uuid'
 import TransactionsHeader from './TransactionsHeader'
 import Transaction from './Transaction'
 
-const buildTransaction = transaction => {
+const buildTransaction = (transaction, removeCallback) => {
   return (
     <li key={uuidv4()}>
       <Transaction {...transaction} />
-      <button className='removeTransaction' onClick={() => console.log('remove')}>Remover</button>
+      <button className='removeTransaction' onClick={removeCallback}>Remover</button>
     </li>
   )
 }
 
-const buildList = transactions => {
+const buildList = (transactions, removeCallback) => {
   return (
     <ul>
-      { transactions.map(transaction => buildTransaction(transaction)) }
+      { transactions.map(transaction => buildTransaction(transaction, removeCallback)) }
     </ul>
   )
 }
 
-const TransactionsList = ({ transactions }) => {
+const TransactionsList = ({ transactions, addCallback, removeCallback }) => {
   return (
     <div>
       <TransactionsHeader />
-      {buildList(transactions)}
-      <button className='addTransaction' onClick={() => console.log('add')}>Adicionar</button>
+      {buildList(transactions, removeCallback)}
+      <button className='addTransaction' onClick={addCallback}>Adicionar</button>
     </div>
   )
 }
 
 TransactionsList.propTypes = {
-  transactions: PropTypes.array.isRequired
+  transactions: PropTypes.array.isRequired,
+  addCallback: PropTypes.func.isRequired,
+  removeCallback: PropTypes.func.isRequired
 }
 
 export default TransactionsList
--- src/components/TransactionsList.test.js
+++ src/components/TransactionsList.test.js
@@ -9,10 +9,21 @@ import mockedData from '../calculator/mockData'
 
 let transactionsList = null
 
+const props = {
+  transactions: mockedData,
+  addCallback: jest.fn(),
+  removeCallback: jest.fn()
+}
+
 describe('Transactions List component', () => {
   beforeAll(() => {
-    transactionsList = shallow(<TransactionsList transactions={mockedData} />)
+    transactionsList = shallow(<TransactionsList { ...props } />)
   })
+  beforeEach(() => {
+    props.addCallback.mockReset()
+    props.removeCallback.mockReset()
+  })
+
   test('renders Transaction Header component', () => {
     expect(transactionsList.find(TransactionsHeader)).toHaveLength(1)
   })
@@ -24,5 +35,15 @@ describe('Transactions List component', () => {
   })
   test('renders add button for a new transaction item', () => {
     expect(transactionsList.find('.addTransaction')).toHaveLength(1)
+  })
+
+  test('execute callback to add new transaction', () => {
+    transactionsList.find('.addTransaction').simulate('click')
+    expect(props.addCallback).toHaveBeenCalledTimes(1)
+  })
+
+  test('execute callback to remove selected transaction', () => {
+    transactionsList.find('.removeTransaction').at(0).simulate('click')
+    expect(props.removeCallback).toHaveBeenCalledTimes(1)
   })
 })
